e14c3b6f30a861588d3a925d3d3620070f96c993
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-17 20:18:41 -0400

    Learn about decorator factories

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-// Usually decorators start with a captial letter
-function Logger(constructor: Function) {
-  console.log("Logging...");
-  console.log(constructor);
+// Usually decorators start with a captial letter, when using a decorator factory it allows us to pass in params to the function
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-// Decorators run when class is defined not when objects are instaniated
-@Logger
+// Decorators run when class is defined not when objects are instaniated. Factories are called with optional params while first class decorators are not
+@Logger("LOGGING - PERSON")
 class Person {
   name = "Max";
 
